4df82544f8ee0cba62d77d6dcffe41d90ec4ada8
LepeRocket <cristoballepegarcia@gmail.com>
2023-12-22 08:17:58 -0300

    Home page styles done

--- src/Components/SocialMediaIcons.jsx
+++ src/Components/SocialMediaIcons.jsx
@@ -1,28 +1,26 @@
 const SocialMediaIcons = () => {
   return (
-    <div className="social-media">
-      <ul className="social-media-list">
-        <li>
-          <a href="https://www.instagram.com/losmaitenesairsoft"  target="_blank" rel="noopener noreferrer">
-            <img className="logo-instagram" src="/images/instagram.png" alt="Instagram" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.tiktok.com/@losmaitenesairsoft"  target="_blank" rel="noopener noreferrer">
-            <img className="logo-tiktok" src="/images/tik-tok.png" alt="TikTok" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/profile.php?id=61554488474975&mibextid=YMEMSu"  target="_blank" rel="noopener noreferrer">
-            <img className="logo-facebook" src="/images/facebook.png" alt="Facebook" />
-          </a>
-        </li>
-        <li>
-          <a href="https://youtube.com/@losmaitenes"  target="_blank" rel="noopener noreferrer">
-            <img className="logo-youtube" src="/images/youtube.png" alt="Youtube" />
-          </a></li>
-      </ul>
-    </div>
+    <ul className="social-media-list">
+      <li>
+        <a href="https://www.instagram.com/losmaitenesairsoft"  target="_blank" rel="noopener noreferrer">
+          <img className="logo-instagram" src="/images/instagram.png" alt="Instagram" />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.tiktok.com/@losmaitenesairsoft"  target="_blank" rel="noopener noreferrer">
+          <img className="logo-tiktok" src="/images/tik-tok.png" alt="TikTok" />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.facebook.com/profile.php?id=61554488474975&mibextid=YMEMSu"  target="_blank" rel="noopener noreferrer">
+          <img className="logo-facebook" src="/images/facebook.png" alt="Facebook" />
+        </a>
+      </li>
+      <li>
+        <a href="https://youtube.com/@losmaitenes"  target="_blank" rel="noopener noreferrer">
+          <img className="logo-youtube" src="/images/youtube.png" alt="Youtube" />
+        </a></li>
+    </ul>
   )
 }
 
