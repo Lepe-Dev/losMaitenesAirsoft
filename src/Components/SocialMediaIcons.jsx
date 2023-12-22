8ab4e34f1dcb46924cfec90ee2d0cbc6f68d096c
LepeRocket <cristoballepegarcia@gmail.com>
2023-12-22 02:00:24 -0300

    Progress on the home page (social media bar)

--- src/Components/SocialMediaIcons.jsx
+++ src/Components/SocialMediaIcons.jsx
@@ -1,12 +1,27 @@
 const SocialMediaIcons = () => {
   return (
     <div className="social-media">
-        <ul className="social-media-list">
-            <li><a href="https://www.instagram.com/losmaitenesairsoft?igshid=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr">Instagram</a></li>
-            <li><a href="#">Tiktok</a></li>
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">Youtube</a></li>
-        </ul>
+      <ul className="social-media-list">
+        <li>
+          <a href="https://www.instagram.com/losmaitenesairsoft"  target="_blank" rel="noopener noreferrer">
+            <img className="logo-instagram" src="/images/instagram.png" alt="Instagram" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.tiktok.com/@losmaitenesairsoft"  target="_blank" rel="noopener noreferrer">
+            <img className="logo-tiktok" src="/images/tik-tok.png" alt="TikTok" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/profile.php?id=61554488474975&mibextid=YMEMSu"  target="_blank" rel="noopener noreferrer">
+            <img className="logo-facebook" src="/images/facebook.png" alt="Facebook" />
+          </a>
+        </li>
+        <li>
+          <a href="https://youtube.com/@losmaitenes"  target="_blank" rel="noopener noreferrer">
+            <img className="logo-youtube" src="/images/youtube.png" alt="Youtube" />
+          </a></li>
+      </ul>
     </div>
   )
 }
