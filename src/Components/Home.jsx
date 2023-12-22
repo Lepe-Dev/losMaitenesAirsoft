d12821558e65468ab72f9c66096f9f1acb1b4621
LepeRocket <cristoballepegarcia@gmail.com>
2023-12-22 09:06:58 -0300

    Home page corrections

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,18 +2,23 @@ import SocialMediaIcons from "./SocialMediaIcons"
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <div className="logo-container">
-        <img className="logo" src="/images/LosMaitenesChile.png" alt="Logo Los Maitenes Airsoft" />
-      </div>
-      <div className="home-information-text">
-        <h1 className="home-title">Bienvenidos a Los Maitenes</h1>
-        <h2 className="home-subtitle">Una experiencia única de Airsoft</h2>
+    <>
+      <div className="home-container">
+        <div className="logo-container">
+          <img className="logo" src="/images/LosMaitenesChile.png" alt="Logo Los Maitenes Airsoft" />
+        </div>
+        <div className="home-information-text">
+          <h1 className="home-title">Bienvenidos a Los Maitenes</h1>
+          <h2 className="home-subtitle">Una experiencia única de Airsoft</h2>
+        </div>
+        <div className="icons-container">
+          <SocialMediaIcons />
+        </div>
       </div>
       <div className="icons-container">
         <SocialMediaIcons />
       </div>
-    </div>
+    </>
   )
 }
 
