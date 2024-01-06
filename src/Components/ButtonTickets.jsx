c9772f225a2cb242a32b2c7c2943bcbc9a81ce17
LepeRocket <cristoballepegarcia@gmail.com>
2024-01-06 13:27:27 -0300

    ticket pop up updated

--- src/Components/ButtonTickets.jsx
+++ src/Components/ButtonTickets.jsx
@@ -6,8 +6,22 @@ const ButtonTickets = () => {
 
         const showSwal = () => {
             withReactContent(Swal).fire({
-                title: <i className='button__tickets__font__size'>here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info here goes the tickets info</i>,
-
+                title:
+                    <div className='ticket__popup'>
+                        <p className='ticket__prices'>
+                            Valor Entrada: $5.000 <br />
+                            Valor Rental: $25.000 {"(Réplica, Chaleco, Máscara, Balines)"}
+                        </p>
+                        <p className='ticket__details'>
+                            Datos Transferencia:<br />
+                            Nombre: Carlos Ortiz<br />
+                            Rut: 11436632-3<br />
+                            Banco: Scotiabank<br />
+                            Tipo Cuenta: Corriente<br />
+                            Número: 988549622<br />
+                        </p><br />
+                        <i className='ticket__info'>Enviar comprobante por correo {'(losmaitenesairsoft@gmail.com)'} o al Whatsapp {'(+56932665359)'} indicando detalle de la compra.</i>
+                    </div>
             })
         }
 
