64a4418f9e535064f073d0a6d2cafa2cad12aede
George <lets.devel0pme@gmail.com>
2024-01-09 16:31:26 -0300

    viteconfig updated x2

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+
+  build: {
+    outDir: 'dist', // Adjust the output directory as needed
+    assetsDir: 'assets', // Adjust the assets directory as needed
+    sourcemap: true, // Enable sourcemaps for debugging in production
+    rollupOptions: {
+      // Ensure proper handling of dynamic imports in production
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
