c3611753600701a25c04c3bee414083eeca837dc
George <lets.devel0pme@gmail.com>
2024-07-27 19:23:23 -0400

    new gallery added

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -9,48 +9,108 @@ import img8 from "../images/buildingStage-8.jpeg";
 /* import img9 from "../images/buildingStage-9.jpeg"; */
 import img10 from "../images/buildingStage-10.jpeg";
 import img11 from "../images/buildingStage-11.jpeg";
-
-
+import LosMaitenes_1 from "../images/los-maitenes-1.png";
+import LosMaitenes_2 from "../images/los-maitenes-2.png";
+import LosMaitenes_3 from "../images/los-maitenes-3.png";
+import LosMaitenes_4 from "../images/los-maitenes-4.png";
+import LosMaitenes_5 from "../images/los-maitenes-5.png";
+import LosMaitenes_6 from "../images/los-maitenes-6.png";
+import LosMaitenes_7 from "../images/los-maitenes-7.png";
+import LosMaitenes_8 from "../images/los-maitenes-8.png";
+import LosMaitenes_9 from "../images/los-maitenes-9.png";
+import LosMaitenes_10 from "../images/los-maitenes-10.png";
+import LosMaitenes_11 from "../images/los-maitenes-11.png";
+import LosMaitenes_12 from "../images/los-maitenes-12.png";
+import LosMaitenes_13 from "../images/los-maitenes-13.png";
 const Gallery = () => {
     return (
         <>
-            <h1 className="gallery__title">- Etapa de construcción</h1>
+            <section>
+            <h1 className="gallery__title">Nuestros clientes</h1>
+                <div className="gallery">
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_1} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_1} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_2} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_3} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_7} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_8} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_9} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_10} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_11} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_4} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_12} alt="los maitenes airsoft image" />
+                    </div>
+                    <div className="gallery__img__container">
+                        <img className="gallery__img" src={LosMaitenes_13} alt="los maitenes airsoft image" />
+                    </div>
+                </div>
+            </section>
+            <div className="gallery">
+                        <div className="gallery__img__container">
+                            <img className="gallery__img" src={LosMaitenes_6} alt="los maitenes airsoft image" />
+                        </div>
+                        <div className="gallery__img__container">
+                            <img className="gallery__img" src={LosMaitenes_5} alt="los maitenes airsoft image" />
+                        </div>
+                    </div>
+            <h2 className="gallery__title">- Etapa de construcción</h2>
             <div className="gallery">
                 <div className="gallery__img__container">
-                    <img className="gallery__img" src={img1} alt="" />
+                    <img className="gallery__img" src={img1} alt="los maitenes airsoft image" />
                 </div>
                 <div className="gallery__img__container">
-                    <img className="gallery__img" src={img2} alt="" />
+                    <img className="gallery__img" src={img2} alt="los maitenes airsoft image" />
                 </div>
                 <div className="gallery__img__container">
-                    <img className="gallery__img" src={img3} alt="" />
+                    <img className="gallery__img" src={img3} alt="los maitenes airsoft image" />
                 </div>
                 {/* <div className="gallery__img__container">
-                    <img className="gallery__img" src={img4} alt="" />
+                    <img className="gallery__img" src={img4} alt="los maitenes airsoft image" />
                 </div> */}
                 <div className="gallery__img__container">
-                    <img className="gallery__img" src={img5} alt="" />
+                    <img className="gallery__img" src={img5} alt="los maitenes airsoft image" />
                 </div>
                 <div className="gallery__img__container">
-                    <img className="gallery__img" src={img6} alt="" />
+                    <img className="gallery__img" src={img6} alt="los maitenes airsoft image" />
                 </div>
                 
                 
                 {/* <div className="gallery__img__container">
-                    <img className="gallery__img" src={img9} alt="" />
+                    <img className="gallery__img" src={img9} alt="los maitenes airsoft image" />
                 </div> */}
                 <div className="gallery__img__container">
-                    <img className="gallery__img" src={img10} alt="" />
+                    <img className="gallery__img" src={img10} alt="los maitenes airsoft image" />
                 </div>
                 <div className="gallery__img__container">
-                    <img className="gallery__img" src={img11} alt="" />
+                    <img className="gallery__img" src={img11} alt="los maitenes airsoft image" />
                 </div>
                 
                 <div className="gallery__img__container">
-                    <img className="gallery__img" src={img7} alt="" />
+                    <img className="gallery__img" src={img7} alt="los maitenes airsoft image" />
                 </div>
                 <div className="gallery__img__container">
-                    <img className="gallery__img" src={img8} alt="" />
+                    <img className="gallery__img" src={img8} alt="los maitenes airsoft image" />
                 </div>
             </div>
         </>
